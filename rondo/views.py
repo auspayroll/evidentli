e314baf7128a4473c3acf1f37fe160b56633334d
Simon Grant <auspayroll@gmail.com>
2019-04-02 13:42:28 +1100

    modified flowfile endpoint, added createRondo endpoint

--- rondo/views.py
+++ rondo/views.py
@@ -17,8 +17,13 @@ def all(project_id):
 @app.route('/projects/<project_id>/rondo', methods=['POST'])
 def createRondo(project_id):
 	posted = request.get_json()
-	rondo = Rondo(project_id=project_id, **posted)
-	rondo.save()
+	_id = posted.get("_id")
+	if _id:
+		rondo = Rondo.get(project_id=project_id, id=_id)
+		rondo.update(**posted)
+	else: 
+		rondo = Rondo(project_id=project_id, **posted)
+		rondo.save()
 	return jsn(rondo._json)
 
 @app.route('/projects/<project_id>/rondo/<config_id>', methods=['GET'])
@@ -26,17 +31,21 @@ def getRondo(project_id, config_id):
 	rondo = Rondo.get(project_id=project_id, id=config_id, json=True)
 	return jsn(rondo)
 
-@app.route('/projects/<project_id>/flowfile/<rondo_id>', methods=['POST'])
+@app.route('/projects/<project_id>/rondo/<rondo_id>/flowfile', methods=['POST'])
 def flowfile(project_id, rondo_id):
 	"""
 	endpoint used for nifi processor. It accepts patient json, 
 	and returns modified json as the flow file.
 	"""
 	json = request.get_json()	
-	patient = Patient.create(**json)
+	patient = Patient.create(_project_id=project_id, **json)
 	rondo = Rondo.get(project_id=project_id, id=rondo_id)
 	rondo.allocate_random_cohort(patient)
 	rondo.match_patient(patient)
-	return jsn(patient._json)
+	if patient.pair_id:
+		json["pair_id"] = patient.pair_id
+	if patient.cohort:
+		json['cohort'] = patient.cohort
+	return jsn(json)
 
 
