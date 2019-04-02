7e7fd7c99540c7935e1c1b3441c3909991d89c04
Michael Van Treeck <michael@evidentli.com>
2019-04-03 09:02:42 +1100

    Comment out assertion in update method in model class PN-110

--- rondo/orm/model.py
+++ rondo/orm/model.py
@@ -123,7 +123,7 @@ class Model(object):
 
 	def update(self, **kwargs):
 		assert 'id' not in kwargs
-		assert '_id' not in kwargs
+		# assert '_id' not in kwargs
 		assert self._id and self._project_id
 		for k, v in kwargs.items():
 			setattr(self, k, v)
