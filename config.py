c59476dcdad7b9e86b97717735c0056a7cce0ed2
Simon Grant <auspayroll@gmail.com>
2019-04-08 13:55:14 +1000

    changes to config setup; allows for local_config.py to override default

--- config.py
+++ config.py
@@ -1,12 +1,25 @@
 import os
 
+"""
+Base Configuration
+"""
+DEBUG = False
+TESTING = False
+CORS = False
+EOBO_KEY = os.getenv('EOBO_KEY', 'eobo')
+SECRET_KEY = os.getenv('SECRET_KEY', "piano")
 PIANO_API = os.getenv('PIANO_API', "http://dev.api.evidentli.com")
+
+try:
+	from local_config import *
+except:
+	pass
 
 
 class Config(object):
-    """
-    Base Configuration
-    """
-    SECRET_KEY = os.getenv('SECRET_KEY', "piano")
-
+    DEBUG = DEBUG
+    TESTING = TESTING
+    CORS = CORS
+    EOBO_KEY = EOBO_KEY
+    SECRET_KEY = SECRET_KEY
     PIANO_API = PIANO_API
