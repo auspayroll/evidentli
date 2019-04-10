7f312a0ec097403f6a94146e76371625eb688d14
Simon Grant <auspayroll@gmail.com>
2019-04-10 12:42:47 +1000

    update match_patient algorithm in Rondo model

--- rondo/rondo_model.py
+++ rondo/rondo_model.py
@@ -8,7 +8,7 @@ from .orm import omop
 
 class Rondo(Model):
     _name = "rondo"
-    _fields = {'cohorts': list, 'matched_pairs': list, 'match_by_cohort': bool, 'name': str}
+    _fields = {'cohorts': list, 'matched_pairs': str, 'match_by_cohort': bool, 'name': str}
 
     def allocate_random_cohort(self, patient):
         # allocat random cohort to patient, deterministically based on _id
@@ -29,52 +29,87 @@ class Rondo(Model):
         for patient in patients:
             self.allocate_random_cohort(patient)
 
-    def _match_patient(self, patient, criteria, pair_id):
+    def _match_patient(self, patient, criteria_dict, pair_id):
         """
         private method to find matching patients and update with pair_id
+        criteria_dict = { 'Person': ['field1', 'field2'], ...}
         """
-        #matched_patients = Patient.filter(project_id=patient._project_id, **criteria)
-        matched_patient_ids = omop.match_patient(patient._project_id, patient.person_id)
-        matched_patients = Patient.match_by_person_ids([match_patient_ids])
+        matched_patient_ids = omop.match_patient(patient._project_id, patient.person_id, criteria_dict)
+        matched_patients = Patient.all_in(project_id=patient._project_id, field='person_id', values=matched_patient_ids)
+        matched_cohorts = [patient.cohort]
+        matched_patient_list = []
         if matched_patients:
             for matched_patient in matched_patients:
                 if matched_patient._id == patient._id:
                     continue
-                if not matched_patient.pair_id:
+                if not matched_patient.pair_id: #skip if already paired
                     matched_patient.pair_id = pair_id
+                    if self.match_by_cohort and matched_patient.cohort not in matched_cohorts:
+                        matched_cohorts.append(matched_patient.cohort)
                     matched_patient.save()
+                    matched_patient_list.append(matched_patient)
                     if patient.pair_id != pair_id:
                         patient.pair_id = pair_id
                         patient.save()
-                    return matched_patient
+                    if not self.match_by_cohort: 
+                        # there are no cohorts, so return only one match
+                        return matched_patient_list
+        return matched_patient_list
 
-    def match_patient(self, patient, matched_pairs=None, match_by_cohort=False):
-        matched_pairs = matched_pairs or self.matched_pairs
-        match_by_cohort = match_by_cohort or self.match_by_cohort
+    @property
+    def _cohort_list(self):
+        split_on = ','
+        if split_on not in self.cohorts:
+            split_on = ' '
+        keywords = [x.strip() for x in self.cohorts.split(split_on)]
+        cohort_list = list(dict.fromkeys(keywords))
+        return cohort_list
 
-        if not matched_pairs: return
-        pair_id = uuid.uuid4().hex
-        find_match = {}
-        for field_match in matched_pairs:
-            field_match_value = getattr(patient, field_match)
-            if field_match_value is None:  # patient has no field
-                continue
-            find_match[field_match] = field_match_value
+    @property
+    def _matched_pairs_dict(self):
+        kw = {}
+        if not self.matched_pairs:
+            return kw
 
-        if match_by_cohort and patient.cohort and self.cohorts:
-            patient_matches = []
-            # find matching pair for each cohort
-            for cohort in self.cohorts:
-                if cohort == patient.cohort:
-                    continue  # bypass patient cohort
-                find_match["cohort"] = cohort
-                matched_patient = self._match_patient(patient, find_match, pair_id)
-                if matched_patient:
-                    patient_matches.append(matched_patient)
-            return patient_matches
-        else:
-            matched_patient = self._match_patient(patient, find_match, pair_id)
-            if matched_patient:
-                return [matched_patient]
+        split_on = ','
+        if split_on not in self.matched_pairs:
+            split_on = ' '
+            
+        keywords = [x.strip() for x in self.matched_pairs.split(split_on)]
+
+        keywords = list(dict.fromkeys(keywords))
+        for keyword in keywords:
+            try:
+                table, field = keyword.split('.')
+            except:
+                pass
             else:
-                return []
+                table_fields = kw.setdefault(table.lower(),[])
+                if field.lower() not in table_fields:
+                    table_fields.append(field.lower())
+        return kw
+
+    @property
+    def _matched_pair_list(self):
+        l = []
+        for k, vl in self._matched_pairs_dict.items():
+            for v in vl:
+                l.append("%s.%s" % (k,v))
+        return l
+
+    def match_patient(self, patient):
+        find_match = self._matched_pairs_dict
+        if not find_match: 
+            return []
+        pair_id = uuid.uuid4().hex
+        matched_patients = self._match_patient(patient, find_match, pair_id)
+        return matched_patients
+
+    def save(self):
+        if 'matched_pairs' in self._field_updates:
+            self.matched_pairs = ', '.join(self._matched_pair_list)
+
+        if 'cohorts' in self._field_updates:
+            self.cohorts = ', '.join(self._cohort_list)
+
+        return super(Rondo, self).save()
