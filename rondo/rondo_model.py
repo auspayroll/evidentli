cd59d29b7f07626f24286e860a5815ae04867072
Simon Grant <auspayroll@gmail.com>
2019-04-15 13:41:21 +1000

    Rondo updated allocate_random_cohort and match_patient methods

--- rondo/rondo_model.py
+++ rondo/rondo_model.py
@@ -8,34 +8,43 @@ from .orm import omop
 
 class Rondo(Model):
     _name = "rondo"
-    _fields = {'cohorts': list, 'matched_pairs': str, 'match_by_cohort': bool, 'name': str}
+    _fields = { 'cohorts': str, 'matched_pairs': str, 'match_by_cohort': bool, 'name': str }
 
-    def allocate_random_cohort(self, patient):
+    def allocate_random_cohort(self, patient, force=False):
         # allocat random cohort to patient, deterministically based on _id
         patient_id = patient._id
         assert patient_id
-        no_cohorts = len(self.cohorts)
-        if self.cohorts and not patient.cohort:
-            # cohort_no = int(hashlib.sha256(str(patient_id).encode("utf-8")).hexdigest(), 16) % no_cohorts
-            cohort_no = randint(0, no_cohorts - 1)
-            allocated_cohort = self.cohorts[cohort_no]
-            patient.cohort = allocated_cohort
-            patient.save()
-            return allocated_cohort
+        no_cohorts = len(self._cohort_list)
+        if self._cohort_list:
+            if not force and not patient.cohort or force:
+                # cohort_no = int(hashlib.sha256(str(patient_id).encode("utf-8")).hexdigest(), 16) % no_cohorts
+                cohort_no = randint(0, no_cohorts - 1)
+                allocated_cohort = self._cohort_list[cohort_no]
+                patient.cohort = allocated_cohort
+                patient.save()
+                return allocated_cohort
+
+    def _reset_pair_ids(self):
+        self._load_patients()
+        for pat in self._patients:
+            pat.cohort = ''
+            pat.save()
 
     def allocate_random_cohorts(self):
         """set random cohorts for all patients, useful for testing"""
-        patients = Patient.all(self._project_id)
-        for patient in patients:
-            self.allocate_random_cohort(patient)
+        self._patients = Patient.all(project_id=self._project_id)
+        for patient in self._patients:
+            self.allocate_random_cohort(patient, force=True)
 
+        
     def _match_patient(self, patient, criteria_dict, pair_id):
         """
+        deprecated
         private method to find matching patients and update with pair_id
         criteria_dict = { 'Person': ['field1', 'field2'], ...}
         """
         matched_patient_ids = omop.match_patient(patient._project_id, patient.person_id, criteria_dict)
-        matched_patients = Patient.all_in(project_id=patient._project_id, field='person_id', values=matched_patient_ids)
+        matched_patients = Patient.filter(project_id=patient._project_id, person_id=matched_patient_ids, omop='Person')
         matched_cohorts = [patient.cohort]
         matched_patient_list = []
         if matched_patients:
@@ -55,6 +64,16 @@ class Rondo(Model):
                         # there are no cohorts, so return only one match
                         return matched_patient_list
         return matched_patient_list
+
+    @property
+    def patient_cohorts(self):
+        self._load_patients()
+        cohort_dict = {}
+        for patient in self._patients:
+            cohort_list = cohort_dict.setdefault(patient.cohort, [])
+            cohort_list.append(patient)
+        return cohort_dict
+
 
     @property
     def _cohort_list(self):
@@ -84,7 +103,7 @@ class Rondo(Model):
             except:
                 pass
             else:
-                table_fields = kw.setdefault(table.lower(),[])
+                table_fields = kw.setdefault(table.capitalize(),[])
                 if field.lower() not in table_fields:
                     table_fields.append(field.lower())
         return kw
@@ -97,6 +116,8 @@ class Rondo(Model):
                 l.append("%s.%s" % (k,v))
         return l
 
+    """
+    deprecated
     def match_patient(self, patient):
         find_match = self._matched_pairs_dict
         if not find_match: 
@@ -104,6 +125,106 @@ class Rondo(Model):
         pair_id = uuid.uuid4().hex
         matched_patients = self._match_patient(patient, find_match, pair_id)
         return matched_patients
+    """
+    def _load_patients(self, limit=None):
+        matched_pairs_dict = self._matched_pairs_dict
+        if type(self._patients) is list:
+            return self._patients
+        else:
+            patients = Patient.filter(project_id=self._project_id, 
+                cohort=self._cohort_list, omop=list(matched_pairs_dict.keys()))
+
+            cohort_counts = {}
+            pats = []
+            for patient in patients:
+                #patient.pair_id = None
+                if limit:
+                    cohort_count = cohort_counts.setdefault(patient.cohort, 0)
+                    if cohort_count < limit:
+                        cohort_counts[patient.cohort] = cohort_count + 1
+                        pats.append(patient)
+                else:
+                    pats.append(patient)
+                #patient._field_updates.clear()
+
+            self._patients = pats
+
+
+    def match_patient(self, patient):
+        self._load_patients()
+        patient_pairs = []
+        if patient.cohort not in self._cohort_list:
+            return []
+
+        if patient.pair_id:
+            return [p for p in self._patients if p.pair_id == patient.pair_id and p._id != patient._id]
+
+        match_pattern = {}
+        for match_field in self._matched_pair_list:
+            tablename, fieldname = match_field.split(".")
+            try:
+                match_pattern[match_field] = getattr(patient, "_%s" % tablename)[fieldname]
+            except:
+                return []
+            else:
+                if not match_pattern[match_field]:
+                    return [] # one of the fields is None, so cant' match
+
+        patient.pair_id = uuid.uuid4().hex
+
+        for cohort, patient_cohort in self.patient_cohorts.items():
+            if cohort == patient.cohort:
+                continue
+            for patient_pair in patient_cohort:
+                if patient_pair.pair_id:
+                    continue
+                broken_match = False
+                for k,v in match_pattern.items():
+                    try:
+                        if getattr(patient, "_%s" % tablename)[fieldname] != getattr(patient_pair, "_%s" % tablename)[fieldname]:
+                            broken_match = True
+                            break
+                    except:
+                        broken_match = True
+                        break
+
+                if not broken_match:
+                    patient_pair.pair_id = patient.pair_id
+                    patient_pair.save()
+                    patient_pairs.append(patient_pair)
+                    if patient._field_updates:
+                        patient.save()
+                    break # match found for cohort
+
+        if 'pair_id' in patient._field_updates:
+            patient.pair_id = None
+            del patient._field_updates['pair_id']
+        return patient_pairs
+
+    @property
+    def matched_patients(self):
+        self._load_patients()
+        for patient in self._patients:
+            patient.pair_id = None
+            patient._field_updates.clear()
+        if not self._patients:
+            return None
+        mpd = {}
+        for pat in self._patients:
+            if pat.pair_id:
+                mpl = mpd.setdefault(pat.pair_id, [])
+                mpl.append(pat)
+        return mpd
+
+    def match_patients(self):
+        #import pdb
+        #pdb.set_trace()
+        matched_pairs_dict = self._matched_pairs_dict
+        self._load_patients()
+        for patient in self._patients:
+            self.match_patient(patient)
+        return self._patients
+
 
     def save(self):
         if 'matched_pairs' in self._field_updates:
