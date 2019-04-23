ae8d8d6478368a442d3c8c065baf2f1b421e6069
Simon Grant <auspayroll@gmail.com>
2019-04-24 09:14:39 +1000

    update stats calculations

--- sumo/sumo_model.py
+++ sumo/sumo_model.py
@@ -5,22 +5,18 @@ from orm.model import Model
 from orm.patient import Patient
 from orm import omop
 import numpy as np
+from . import utils
 
 
 class Sumo(Model):
     _name = "sumo"
 
     def analyse(self):
-        self.stats = { "cohorts" : { key: { k: { "mean": None, "std": None, "median": None, "iqr": None } 
-                        for k in self._field_list } for key in self._cohort_list }}
-
         self._patient_values = { key: { k: [] for k in self._field_list } for key in self._cohort_list }
         self._load_patients()
-        self._calc_stats()
-        cohort_list = self._cohort_list
-        if cohort_list and len(cohort_list) > 1:
-            self.stats["comparison"] = self.compare_cohorts(cohort_list[0], cohort_list[1])
-            self.stats["matched_pairs"] = self.compare_cohort_matched_pairs(cohort_list[0], cohort_list[1])
+        if self._patients:
+            self._calc_stats()
+            self.save()
 
         return self.stats
 
@@ -39,9 +35,11 @@ class Sumo(Model):
             field_dict[field_name]['matched_pairs'] = field_stats   
 
         for field_name, field_stats in self.stats["comparison"].items():
-            field_dict[field_name]['comparison'] = field_stats     
+            field_dict[field_name]['comparison'] = field_stats 
 
-        return field_dict
+        stats = { "fields": field_dict, "categorized": self.stats['categorized'] }    
+
+        return stats
 
 
     def compare_cohorts(self, cohort1, cohort2):
@@ -49,12 +47,19 @@ class Sumo(Model):
             raise Exception("stats not defined")
 
         summary = { k: {} for k in self._field_list }
-        for item in self._field_list:
-            summary[item]['mean'] = self.stats['cohorts'][cohort1][item]['mean'] - self.stats['cohorts'][cohort2][item]['mean']
-            summary[item]['or'] = self.stats['cohorts'][cohort1][item]['or'] - self.stats['cohorts'][cohort2][item]['or']
-            summary[item]['median'] = self.stats['cohorts'][cohort1][item]['median'] - self.stats['cohorts'][cohort2][item]['median']
-            summary[item]['std'] = self.stats['cohorts'][cohort1][item]['std'] - self.stats['cohorts'][cohort2][item]['std']
-            summary[item]['iqr'] = self.stats['cohorts'][cohort1][item]['iqr'] - self.stats['cohorts'][cohort2][item]['iqr']
+        for item in self._field_list:        
+            try:
+                summary[item]['OR'] = self.stats['cohorts'][cohort1][item]['ratio'] / self.stats['cohorts'][cohort2][item]['ratio']
+            except:
+                summary[item]['OR'] = None
+
+            try:
+                summary[item]['mean'] = self.stats['cohorts'][cohort1][item]['mean'] - self.stats['cohorts'][cohort2][item]['mean']
+                summary[item]['median'] = self.stats['cohorts'][cohort1][item]['median'] - self.stats['cohorts'][cohort2][item]['median']
+                summary[item]['std'] = self.stats['cohorts'][cohort1][item]['std'] - self.stats['cohorts'][cohort2][item]['std']
+                summary[item]['iqr'] = self.stats['cohorts'][cohort1][item]['iqr'] - self.stats['cohorts'][cohort2][item]['iqr']
+            except: #probably a nominal field
+                pass
         return summary
 
 
@@ -70,57 +75,115 @@ class Sumo(Model):
 
         for pat1, pat2 in matched_pairs:
             for item in self._field_list:
-                table, field = item.split('.')
-                value1 = getattr(pat1, "_%s" % (table)).get(field)
-                value2 = getattr(pat2, "_%s" % (table)).get(field)
-                diff = value1 - value2
-                summary[item].append(diff)
+                table, field = item.split('__')
+                try:
+                    value1 = getattr(pat1, "_%s" % (table)).get(field)
+                    value2 = getattr(pat2, "_%s" % (table)).get(field)
+                except:
+                    pass
+                try:
+                    diff = value1 - value2
+                except: #probably a nominal field
+                    continue
+                else:
+                    summary[item].append(diff)
 
         stats = {}
         for field, diffs in summary.items():
-            stats[field] = {}
-            stats[field]['mean'] = np.mean(diffs)
-            stats[field]['median'] = np.median(diffs)
-            stats[field]['std'] = np.std(diffs)
-            stats[field]['or'] = 1
-            stats[field]['iqr'] = np.subtract(*np.percentile(diffs, [75, 25]))
+            if diffs:
+                stats[field] = {}
+                stats[field]['mean'] = np.mean(diffs)
+                stats[field]['median'] = np.median(diffs)
+                stats[field]['std'] = np.std(diffs)
+                stats[field]['n'] = len(diffs)
+                stats[field]['iqr'] = np.subtract(*np.percentile(diffs, [75, 25]))
 
         return stats
 
 
-    def _calc_stats(self):
-        no_patients = len(self._patients)
-        for table, fields in self._field_dict.items():
-            for field in fields:
-                for patient in self._patients:
-                    try:
-                        value = getattr(patient, "_%s" % (table)).get(field)
-                        
-                    except TypeError:
-                        pass
-                    else:
-                        if value:
-                            self._patient_values[patient.cohort]["%s.%s" % (table, field)].append(value)
+    @property
+    def category_list(self):
+        cats = utils._split_string(self.categories)
+        try:
+            cats.sort()
+        except:
+            return None
+        else:
+            return cats
 
-        for table, fields in self._field_dict.items():
-            for field in fields:
-                for patient in self._patients:
-                    values = self._patient_values[patient.cohort]["%s.%s" % (table, field)]
-                    if values:
-                        self.stats['cohorts'][patient.cohort]["%s.%s" % (table, field)]['mean'] = np.mean(values)
-                        self.stats['cohorts'][patient.cohort]["%s.%s" % (table, field)]['std'] = np.std(values)
-                        self.stats['cohorts'][patient.cohort]["%s.%s" % (table, field)]['iqr'] = np.subtract(*np.percentile(values, [75, 25]))
-                        self.stats['cohorts'][patient.cohort]["%s.%s" % (table, field)]['median'] = np.median(values)
-                        self.stats['cohorts'][patient.cohort]["%s.%s" % (table, field)]['or'] = 1
+
+    def _calc_stats(self):
+        categorized = {}
+        self.stats = { "categorized": [], "cohorts" : { key: { k: { "mean": None, "std": None, "median": None, "iqr": None, 
+                    "ratio": None, "n": 0, 'exposures': 0 } for k in self._field_list } for key in self._cohort_list }}
+        no_patients = len(self._patients)
+        for field in self._field_list:
+            table, fieldname = field.split('__')
+            for patient in self._patients:
+                try:
+                    value = float(getattr(patient, "_%s" % (table)).get(fieldname))
+                    
+                except: 
+                    try: #value is a string 
+                        value = getattr(patient, "_%s" % (table)).get(fieldname) 
+                    except: #value is probably non-exisitent
+                        value = None
+                    else: # auto categorize + set exposure
+                        self.stats['cohorts'][patient.cohort][field]['n'] += 1
+                        if value == self.exposure_level:
+                            self.stats['cohorts'][patient.cohort][field]['exposures'] += 1
+                        cat_counts = categorized.setdefault(value, 0)
+                        categorized[value] = cat_counts + 1
+
+                else: #value is numeric
+                    self._patient_values[patient.cohort][field].append(value)
+                    if value:
+                        if self.exposure_level:
+                            try:
+                                if value >= float(self.exposure_level):
+                                    self.stats['cohorts'][patient.cohort][field]['exposures'] += 1
+                            except:
+                                pass
+
+                        else:
+                            self.stats['cohorts'][patient.cohort][field]['exposures'] += 1
+
+                    if self.category_list:
+                        category = utils.categorize_by_list(self.category_list, value)
+                        cat_counts = categorized.setdefault(category, 0)
+                        categorized[category] += 1
+
+        self.stats["categorized"] = [(k, v) for k, v in categorized.items()]
+        self.stats["categorized"].sort(key=lambda x: list(map(lambda y: (float(y[1]),y[0]), [x[0].split(' ')]))[0])
+        print(self.stats["categorized"])
+
+        for field in self._field_list:
+            for patient in self._patients:
+                values = self._patient_values[patient.cohort][field]
+
+                if values:
+                    self.stats['cohorts'][patient.cohort][field]['mean'] = np.mean(values)
+                    self.stats['cohorts'][patient.cohort][field]['std'] = np.std(values)
+                    self.stats['cohorts'][patient.cohort][field]['iqr'] = np.subtract(*np.percentile(values, [75, 25]))
+                    self.stats['cohorts'][patient.cohort][field]['median'] = np.median(values)  
+                    self.stats['cohorts'][patient.cohort][field]['n'] = len(values)
+
+                n = self.stats['cohorts'][patient.cohort][field]['n']
+                if n:
+                    exposures = self.stats['cohorts'][patient.cohort][field]['exposures']
+                    self.stats['cohorts'][patient.cohort][field]['ratio'] = exposures / n
+
+        cohort_list = self._cohort_list
+        if cohort_list and len(cohort_list) > 1:
+            self.stats["comparison"] = self.compare_cohorts(cohort_list[0], cohort_list[1])
+            self.stats["matched_pairs"] = self.compare_cohort_matched_pairs(cohort_list[0], cohort_list[1])
+
+            
+
 
     @property
     def _cohort_list(self):
-        split_on = ','
-        if split_on not in self.cohorts:
-            split_on = ' '
-        keywords = [x.strip() for x in self.cohorts.split(split_on)]
-        cohort_list = list(dict.fromkeys(keywords))
-        return cohort_list
+        return utils._split_string(self.cohorts)
 
 
     def _load_patients(self, limit=None):
@@ -146,26 +209,21 @@ class Sumo(Model):
 
             self._patients = pats
 
-
     @property
     def _field_dict(self):
         kw = {}
         if not self.foa:
             return kw
 
-        split_on = ','
-        if split_on not in self.foa:
-            split_on = ' '
-            
-        keywords = [x.strip() for x in self.foa.split(split_on)]
-
-        keywords = list(dict.fromkeys(keywords))
+        keywords = utils._split_string(self.foa.replace('.','__'))
         for keyword in keywords:
             try:
-                table, field = keyword.split('.')
+                table, field = keyword.split('__')
             except:
                 pass
             else:
+                table = utils.sanitize_key(table)
+                field = utils.sanitize_key(field)
                 table_fields = kw.setdefault(table.capitalize(),[])
                 if field.lower() not in table_fields:
                     table_fields.append(field.lower())
@@ -176,7 +234,7 @@ class Sumo(Model):
         l = []
         for k, vl in self._field_dict.items():
             for v in vl:
-                l.append("%s.%s" % (k,v))
+                l.append("%s__%s" % (k,v))
         return l
 
 
