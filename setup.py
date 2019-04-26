12fddf2d76a6af27eb1737a003031278b3e8c919
Simon Grant <auspayroll@gmail.com>
2019-04-26 17:35:11 +1000

    modify setup.py to self discover packages; add requirement.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='rondo',
-    packages=['rondo'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask', 'requests', 'unittest', 'python-string-utils',
+        'flask', 'requests', 'python-string-utils',
 	'numpy'
     ],
 )
