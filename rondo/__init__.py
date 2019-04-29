df6eba1e142440ab1663325b7420770da5fa4bf1
Simon Grant <auspayroll@gmail.com>
2019-04-29 12:44:06 +1000

    expose Rondo and Sumo classes in respective packages directly

--- rondo/__init__.py
+++ rondo/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-
 app.config.from_object('config.Config')
-
 import rondo.views
+from .rondo_model import Rondo
--- sumo/__init__.py
+++ sumo/__init__.py
@@ -1,4 +1,4 @@
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config.Config')
-from . import views
+from .sumo_model import Sumo
