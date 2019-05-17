4e2d0c29200532af82782220e8b3c79af7788b59
Michael Van Treeck <michael@evidentli.com>
2019-05-17 11:46:36 +1000

    Fix up Sumo tests and add test for api calls PN-185

--- sumo/test/test.py
+++ sumo/test/test.py
@@ -1,14 +1,79 @@
 import unittest
-from orm import Patient, omop
-from sumo.sumo_model import Sumo
+from os import getenv
+
 import numpy as np
+import requests
+
+from config import PIANO_API
+from orm import Patient
+from sumo.sumo_model import Sumo
+
+DELETE_AUTH_KEY = getenv('DELETE_AUTH_KEY')
+EUPHO_AUTH_KEY = getenv("EUPHO_AUTH_KEY", "eupho")
+OBOE_AUTHKEY = getenv("OBOE_AUTH_KEY", "eobo")
 
 
 class TestSumo(unittest.TestCase):
+    project_id = "test_sumo_integration"
+    project_id_api = "test_sumo_integration_api"
+    project_patients = []
+
+    @staticmethod
+    def create_project(project_id, project, eupho=None):
+        # create project
+        url = "%s/projects/%s/projectdata" % (PIANO_API, project_id)
+        requests.post(url, json=[project])
+
+        # create eupho config
+        if eupho:
+            url = "%s/projects/%s/eupho" % (PIANO_API, project_id)
+            requests.post(url, json=[eupho], headers={"EUPHO-AUTHKEY": EUPHO_AUTH_KEY})
+
+            # set oboe role
+            requests.put("%s/projects/%s/omop/role" % (PIANO_API, project_id), headers={"AUTHKEY": OBOE_AUTHKEY})
+
+            # refresh documents (patients)
+            r = requests.get("%s/projects/%s/patients/refresh" % (PIANO_API, project_id))
+            if r.status_code == requests.codes.ok:
+                return r.json()
+        return []
+
+    @staticmethod
+    def delete_project(project_id):
+        url = "%s/projects/%s" % (PIANO_API, project_id)
+        requests.delete(url, headers={"AUTHKEY": DELETE_AUTH_KEY})
+
+    @classmethod
+    def setUpClass(cls):
+        # delete/clear test project(s)
+        cls.delete_project(cls.project_id)
+        cls.delete_project(cls.project_id_api)
+
+        # create test project(s)
+        project_id = cls.project_id_api
+        cls.project = {
+            "_id": project_id,
+            "type": "omop",
+        }
+        cls.project_eupho = {
+            "_id": project_id,
+            "project_type": "patients",
+            "db_version": "v2018_syn_00",
+            "table_permissions": ["condition_occurrence", "death", "drug_exposure", "measurement", "observation",
+                                  "person", "procedure_occurrence", "provider", "specimen", "visit_occurrence"],
+            "oboe_role": "%s_role" % project_id
+        }
+        cls.project_patients = cls.create_project(project_id, cls.project, cls.project_eupho)
+
+        cls.create_project(cls.project_id, {"_id": cls.project_id, "type": "omop"})
+
+    @classmethod
+    def tearDownClass(cls):
+        # delete/clear test project(s)
+        cls.delete_project(cls.project_id_api)
 
     def setUp(self):
-        self.project_id = 'test_rondo_integration'
-        self.foa = "Person__day_of_birth"
+        self.foa = "Person.day_of_birth"
         self.patients = {
             "1": { "cohort": 'A', "pair_id": '3454354WWWW', 'test_val': 7 }, 
             "2": { "cohort": 'B', "pair_id": '3454354WWWW', 'test_val': 12 },
@@ -16,12 +81,11 @@ class TestSumo(unittest.TestCase):
             "4": { "cohort": 'B', "pair_id": '3454354XXXX', 'test_val': 6 },
         }
 
-        self.sumo = sumo = Sumo(project_id=self.project_id, cohorts='A,B', foa=self.foa)
-        sumo._patients = [ Patient.create(_project_id=self.project_id, _id=k, _Person={ self.foa.split('__')[1]: v['test_val']}, **v) for k,v in self.patients.items()]
-        
+        self.sumo = sumo = Sumo.create(_project_id=self.project_id, _id="test", cohorts='A,B', foa=self.foa)
+        sumo._patients = [ Patient.create(_project_id=self.project_id, _id=k, _Person={ self.foa.split('.')[1]: v['test_val']}, **v) for k,v in self.patients.items()]
 
     def tearDown(self):
-        pass
+        self.delete_project(self.project_id)
 
     def test_stats_mean(self):
         sumo = self.sumo
@@ -30,7 +94,6 @@ class TestSumo(unittest.TestCase):
         assert(predicted_mean_for_cohort_a == sumo.stats['cohorts']['A'][self.foa]['mean'])
         predicted_mean_for_cohort_b = np.mean([pat['test_val'] for _id, pat in self.patients.items() if pat['cohort'] == 'B'])
         assert(predicted_mean_for_cohort_b == sumo.stats['cohorts']['B'][self.foa]['mean'])
-    
 
     def test_stats_std(self):
         sumo = self.sumo
@@ -56,7 +119,6 @@ class TestSumo(unittest.TestCase):
         expected_mean_diff = abs(predicted_mean_for_cohort_a - predicted_mean_for_cohort_b)
         assert(abs(cohort_stats[self.foa]['mean']) == abs(expected_mean_diff))
 
-
     def test_compare_cohorts_std(self):
         self.sumo.analyse()
         cohort_stats = self.sumo.compare_cohorts('A', 'B')
@@ -65,7 +127,7 @@ class TestSumo(unittest.TestCase):
         expected_std_diff = abs(predicted_std_for_cohort_a - predicted_std_for_cohort_b)
         assert(abs(cohort_stats[self.foa]['std']) == abs(expected_std_diff))
 
-    def test_compare_cohorts_mean(self):
+    def test_compare_cohorts_median(self):
         self.sumo.analyse()
         cohort_stats = self.sumo.compare_cohorts('A', 'B')
         predicted_median_for_cohort_a = np.median([pat['test_val'] for _id, pat in self.patients.items() if pat['cohort'] == 'A'])
@@ -98,11 +160,10 @@ class TestSumo(unittest.TestCase):
         sumo = self.sumo
         sumo.categories = '11,7'
         sumo.analyse()
-        distribution = self.sumo.stats['distribution'][self.foa]
+        distribution = self.sumo.stats['distribution'][self.foa]["total"]
         assert [v for (k,v) in distribution if k == '< 7.0'][0] == 2
         assert [v for (k,v) in distribution if k == '> 11.0'][0] == 1
         assert [v for (k,v) in distribution if k == '<= 11.0'][0] == 1
-
 
     def test_odds_cateogries_nominal(self):
         patients = {
@@ -113,9 +174,9 @@ class TestSumo(unittest.TestCase):
             "5": { "cohort": 'A', "pair_id": '', 'test_val': 'A' },
         }
         self.sumo._patients = [ Patient.create(_project_id=self.project_id, _id=k,
-            _Person={ self.foa.split('__')[1]: v['test_val']}, **v) for k,v in patients.items()]
+            _Person={ self.foa.split('.')[1]: v['test_val']}, **v) for k,v in patients.items()]
         self.sumo.analyse()
-        distribution = self.sumo.stats['distribution'][self.foa]
+        distribution = self.sumo.stats['distribution'][self.foa]["total"]
         assert [v for (k,v) in distribution if k == 'A'][0] == 3
         assert [v for (k,v) in distribution if k == 'B'][0] == 1
 
@@ -128,10 +189,10 @@ class TestSumo(unittest.TestCase):
             "5": { "cohort": 'A', "pair_id": '', 'test_val': 'A' },
         }
         self.sumo._patients = [ Patient.create(_project_id=self.project_id, _id=k,
-            _Person={ self.foa.split('__')[1]: v['test_val']}, **v) for k,v in patients.items()]
+            _Person={ self.foa.split('.')[1]: v['test_val']}, **v) for k,v in patients.items()]
         self.sumo.exposure_level = 'A'
         self.sumo.analyse()
-        assert self.sumo.stats['comparison'][self.foa]['OR'] == (2.0/3.0) / (1.0/2.0)
+        assert self.sumo.stats['comparison']["A - B"][self.foa]['OR'] == (2.0/3.0) / (1.0/2.0)
 
     def test_odds_ratio_numeric(self):
         patients = {
@@ -142,11 +203,49 @@ class TestSumo(unittest.TestCase):
             "5": { "cohort": 'A', "pair_id": '', 'test_val': 5 },
         }
         self.sumo._patients = [ Patient.create(_project_id=self.project_id, _id=k,
-            _Person={ self.foa.split('__')[1]: v['test_val']}, **v) for k,v in patients.items()]
+            _Person={ self.foa.split('.')[1]: v['test_val']}, **v) for k,v in patients.items()]
         self.sumo.exposure_level = 3
         self.sumo.analyse()
-        assert self.sumo.stats['comparison'][self.foa]['OR'] == (2.0/3.0) / (1.0/2.0)
-    
+        assert self.sumo.stats['comparison']["A - B"][self.foa]['OR'] == (2.0/3.0) / (1.0/2.0)
+
+    def test_sumo_api(self):
+        # create config
+        url = "%s/projects/%s/sumo" % (PIANO_API, self.project_id_api)
+        config_id = "sumo_config_1"
+        cohorts = ["A", "B"]
+        config = {
+            "_id": config_id,
+            "name": "",
+            "cohorts": ", ".join(cohorts),
+            "exposure_level": "french",
+            "foa": "Person.ethnicity_source_value",
+            "categories": ""
+        }
+        requests.post(url, json=[config])
+
+        # assign patients to cohorts
+        patients = []
+        cohort_len = len(cohorts)
+        _range = 1000
+        if len(self.project_patients) < _range:
+            _range = len(self.project_patients)
+        for i in range(_range):
+            patients.append({
+                "_id": self.project_patients[i]["_id"],
+                "cohort": cohorts[i % cohort_len]
+            })
+        requests.post("%s/projects/%s/patients" % (PIANO_API, self.project_id_api), json=patients)
+
+        # test api
+        url = "%s/projects/%s/sumo/%s/calc_stats" % ("http://localhost:5011", self.project_id_api, config_id)
+        r = requests.get(url)
+        self.assertEquals(r.status_code, requests.codes.ok)
+        self.assertTrue(r.content)
+        _json = r.json()
+
+        self.assertTrue("distribution" in _json)
+        self.assertTrue("fields" in _json)
+
 
 if __name__ == '__main__':
     unittest.main()
